139717c49f4df3e9f56c5167ead31fcde2ae1e27
jdsig <jdsig@LAPTOP-I6QV270M>
2026-05-05 12:20:31 +0200

    fix: move the category selector right above the Start/Stop Work buttons

--- src/DoneLogger/Forms/MainForm.Designer.cs
+++ src/DoneLogger/Forms/MainForm.Designer.cs
@@ -49,36 +49,36 @@ partial class MainForm
         lblElapsed.Font = new Font(Font.FontFamily, 10F, FontStyle.Bold);
         lblElapsed.Text = string.Empty;
 
-        // lblCategoryLabel
-        lblCategoryLabel.AutoSize = true;
-        lblCategoryLabel.Location = new Point(10, 70);
-        lblCategoryLabel.Text = "Category:";
-
-        // cboCategory
-        cboCategory.DropDownStyle = ComboBoxStyle.DropDownList;
-        cboCategory.Location = new Point(80, 67);
-        cboCategory.Size = new Size(260, 23);
-
         // btnCreateLog
-        btnCreateLog.Location = new Point(10, 104);
+        btnCreateLog.Location = new Point(10, 70);
         btnCreateLog.Size = new Size(160, 32);
         btnCreateLog.Text = "Create New Log";
         btnCreateLog.Click += btnCreateLog_Click;
 
         // btnEditLog
-        btnEditLog.Location = new Point(180, 104);
+        btnEditLog.Location = new Point(180, 70);
         btnEditLog.Size = new Size(160, 32);
         btnEditLog.Text = "Edit Latest Log";
         btnEditLog.Click += btnEditLog_Click;
 
+        // lblCategoryLabel
+        lblCategoryLabel.AutoSize = true;
+        lblCategoryLabel.Location = new Point(10, 116);
+        lblCategoryLabel.Text = "Category:";
+
+        // cboCategory
+        cboCategory.DropDownStyle = ComboBoxStyle.DropDownList;
+        cboCategory.Location = new Point(80, 113);
+        cboCategory.Size = new Size(260, 23);
+
         // btnStartWork
-        btnStartWork.Location = new Point(10, 146);
+        btnStartWork.Location = new Point(10, 148);
         btnStartWork.Size = new Size(160, 35);
         btnStartWork.Text = "Start Work";
         btnStartWork.Click += btnStartWork_Click;
 
         // btnStopWork
-        btnStopWork.Location = new Point(180, 146);
+        btnStopWork.Location = new Point(180, 148);
         btnStopWork.Size = new Size(160, 35);
         btnStopWork.Text = "Stop Work";
         btnStopWork.Click += btnStopWork_Click;
